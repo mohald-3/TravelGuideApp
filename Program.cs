1559bc1bbd86a5e8d00b738ff7453b16a2647c04
Mohanad Al-Daghestani <mohanad.aldaghestani@gmail.com>
2024-12-09 09:20:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelGuideApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
